990f7aca869e95e1537435259fd898d7d399ed1e
OsimFavour <famos204@gmail.com>
2025-03-18 05:58:50 +0100

    Modular with hooks

--- src/components/shipping.tsx
+++ src/components/shipping.tsx
@@ -1,34 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useShippingMethods } from "../hooks/useShippingMethods";
 
-type ShippingMethod = {
+export type ShippingMethod = {
   provider: string;
   label: string;
 };
 
 const Shipping = ({ cost }: { cost: number }) => {
-  const [shippingMethods, setShippingMethods] = useState<ShippingMethod[]>([]);
-  const [selectedMethod, setSelectedMethod] = useState<string>("standard");
-
-  useEffect(() => {
-    const fetchShippingMethods = async () => {
-      try {
-        const response = await fetch("https://67d8ebee00348dd3e2a8b9f0.mockapi.io/api/v1/shipping-methods");
-        const methods = await response.json();
-
-        const formattedMethods: ShippingMethod[] = methods.map((method: { name: string }) => ({
-          provider: method.name,
-          label: `Ship via ${method.name}`,
-        }));
-
-        formattedMethods.push({ provider: "standard", label: "Standard Shipping" });
-        setShippingMethods(formattedMethods);
-      } catch (error) {
-        console.error("Error fetching shipping methods", error);
-      }
-    };
-
-    fetchShippingMethods();
-  }, []);
+    const [selectedMethod, setSelectedMethod] = useState("standard");
+    
+    const { shippingMethods } = useShippingMethods()
 
   return (
     <>
---
+++ src/hooks/useShippingMethods.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { ShippingMethod } from "../components/shipping";
+
+export const useShippingMethods = () => {
+  const [shippingMethods, setShippingMethods] = useState<ShippingMethod[]>([]);
+
+  useEffect(() => {
+    const fetchShippingMethods = async () => {
+      try {
+        const response = await fetch(
+          "https://67d8ebee00348dd3e2a8b9f0.mockapi.io/api/v1/shipping-methods"
+        );
+        const methods = await response.json();
+
+        const formattedMethods: ShippingMethod[] = methods.map(
+          (method: { name: string }) => ({
+            provider: method.name,
+            label: `Ship via ${method.name}`,
+          })
+        );
+
+        formattedMethods.push({
+          provider: "standard",
+          label: "Standard Shipping",
+        });
+        setShippingMethods(formattedMethods);
+      } catch (error) {
+        console.error("Error fetching shipping methods", error);
+      }
+    };
+
+    fetchShippingMethods();
+  }, []);
+
+  return { shippingMethods };
+};
